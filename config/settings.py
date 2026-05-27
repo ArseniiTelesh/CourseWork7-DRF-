938b44cf3563a70d4d5d9f0dd84af6cd4935f744
ArseniiTelesh <telesh.sen98@gmail.com>
2026-05-27 23:43:30 +0300

    CourseWork7 with Docker now!

--- config/settings.py
+++ config/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "drf_yasg",
     "rest_framework",
     "rest_framework_simplejwt",
     "django_celery_beat",
     "corsheaders",
+
     "users",
     "habits",
 ]
@@ -80,9 +82,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("ENGINE"),
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
